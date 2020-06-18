c39bce6c57a3c6a51cf41f301563e6c29f6d1f4e
Mica Tonge <micatonge12@gmail.com>
2020-06-18 10:26:14 -0400

    Update RegisterComponent.js

--- src/RegisterComponent.js
+++ src/RegisterComponent.js
@@ -4,26 +4,17 @@ import UserService from "./UserService"
 
 class RegisterComponent extends React.Component {
   state = {
-<<<<<<< HEAD
     username: ''
   }
 
   componentDidMount() {
     console.log(UserService.users)
-=======
-    username: '',
-    password: ''
-  }
-
-  componentDidMount() {
->>>>>>> 704fbb55e30139e7225773f1705adaf30673b0d6
   }
 
   render() {
     return (
       <div id= "pie">
         <h1>Register</h1>
-<<<<<<< HEAD
         <input 
           onChange={(event) => {
             const userInput = event.target.value
@@ -37,27 +28,6 @@ class RegisterComponent extends React.Component {
           Register
         </button>
         <Link  id = "ok" to='/'>
-=======
-        <input
-          className="form-control"
-          onChange={(event) => {this.setState({username: event.target.value})}}
-          value={this.state.username}
-          placeholder="Username"/>
-        <input
-          className="form-control"
-          onChange={(event) => {this.setState({password: event.target.value})}}
-          value={this.state.password}
-          placeholder="Password"/>
-        <button 
-          className="btn btn-primary"
-          onClick={() => {
-            UserService.addUser(this.state.username, this.state.password)
-            .then(status => this.props.history.push('/profile/' + this.state.username))
-          }}>
-          Register
-        </button>
-        <Link to='/'>
->>>>>>> 704fbb55e30139e7225773f1705adaf30673b0d6
           Back
         </Link>
       </div>
